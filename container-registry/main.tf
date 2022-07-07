d0f34670d9e02008175110bbd725b689ac5ed402
irupeshdhondi <irupesh.dhondi@gmail.com>
2022-07-07 09:56:51 +0530

    commented added by code

--- container-registry/main.tf
+++ container-registry/main.tf
@@ -1,3 +1,4 @@
+# Acr resource creating by terraform code
 resource "azurerm_container_registry" "acr" {
   name                     = "${var.acr_name}${random_string.myrandom.id}"
   resource_group_name      = "${var.resource_group}_rg"
